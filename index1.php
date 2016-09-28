d17408240b7666b323e7c4c8dc0b2164af4b4c3a
Kabilan Senguttuvan <skabythewebdev20@gmail.com>
2016-09-28 15:37:06 +0530

    Total Confusion

--- index1.php
+++ index1.php
@@ -35,3 +35,17 @@ Confirm Password:<input type="password" name="paswrdcnfrm"><br><br>
 </div> <!--first-->
 </body>
 </html>
+<?php
+
+$conn=mysqli_connect("localhost");
+if(!$conn)
+{
+	die('error in connection');
+}
+
+$username= $_POST['usrname'];
+$ph=$_POST['number'];
+$pass=$_POST['paswrd'];
+$sql1= "INSERT INTO userdb(username,phone,password) VALUES('$username'.$ph,'$pass')";
+mysqli_query($sql1);
+?>
--- sql.php
+++ sql.php
@@ -3,10 +3,12 @@
 $conn=mysqli_connect("localhost");
 if(!$conn)
 {
-	die('error in connection'.mysql_error());
+	die('error in connection');
 }
-mysqli_select_db(sign);
-$sql1= INSERT INTO userdb(username,password,phone) VALUES (.'$_POST[usrname]'.,.'$_POST[number]'.,.'$_POST[paswrd]'.);
-mysqli_query($sql1,$conn);
-mysqli_close($conn);
+
+$username= $_POST["usrname"];
+$ph=$_POST["number"];
+$pass=$_POST["paswrd"];
+$sql1= "INSERT INTO userdb(username,phone,password) VALUES('$username'.$ph,'$pass')";
+mysqli_query($userdb,$sql1);
 ?>
